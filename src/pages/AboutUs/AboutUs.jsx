6b8241f73770b711ab7f2bf4d65dadd3c271ac79
reza niakani <reza.h.niakani.1375@gmail.com>
2025-06-09 16:16:29 +0330

    add some new color

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function AboutUs() {
-  return <div className="w-full"></div>;
+  return (
+    <div className="  bg-[url('asset/Pictures/BackGround/landingBg.svg')]  bg-cover w-screen  bg-no-repeat  bg-center h-[600px] flex flex-col justify-center  items-end lg:py-0 py-6  ">
+      <span> درباره تامین سرمایه سپهر </span>
+    </div>
+  );
 }
 
 export default AboutUs;
